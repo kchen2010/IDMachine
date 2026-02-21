52f3afb0c955e521ca47e567ed5e8f2d490e48f5
Kwon-Yu <kmattchen2010@gmail.com>
2026-02-21 00:05:18 -0800

    initial project setup

---
+++ src/main/java/com/kchen2010/idmachine/IdMachineApplication.java
@@ -0,0 +1,13 @@
+package com.kchen2010.idmachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdMachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdMachineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kchen2010/idmachine/IdMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kchen2010.idmachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdMachineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
